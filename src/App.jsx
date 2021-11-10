baaf2fd3ea069217c834a26a998ace8941a6a7a5
Michael Košatka <michael.kosatka@hrdlicka.cz>
2021-11-10 15:19:47 +0100

    agregate app state to a single object with useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React, { useState } from 'react'
-import './styles/app.css'
+import './styles/app.css';
+import React, { useReducer } from 'react';
+
+import {GameBoard} from './components/GameBoard.jsx';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'set-width':
+      return {...state, width: action.payload};
+    case 'set-height':
+      return {...state, height: action.payload};
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [width, setWidth] = useState(10);
-  const [height, setHeight] = useState(10);
-
-  const renderCells = ({width, height}) => {
-    const cells = [];
-    for (let cell = 0; cell < width * height; cell++) {
-      cells.push(<div className="cell" key={cell}></div>)
-    }
-    return cells;
-  }
+  const [state, dispatch] = useReducer(reducer, {width: 10, height: 10});
 
   return (
     <div className="app">
       <div className="control-panel">
-        <label for="board-width">Board width:</label>
-        <input type="number" name="board-width" value={width} onChange={e => setWidth(e.target.value)} />
-        <label for="board-height">Board height:</label>
-        <input type="number" name="board-height" value={height} onChange={e => setHeight(e.target.value)} />
+        <label htmlFor="board-width">Board width:</label>
+        <input type="number" name="board-width" value={state.width} onChange={e => dispatch({type: 'set-width', payload: e.target.value})} />
+        <label htmlFor="board-height">Board height:</label>
+        <input type="number" name="board-height" value={state.height} onChange={e => dispatch({type: 'set-height', payload: e.target.value})} />
       </div>
-      <div className="game-board" style={{gridTemplateColumns: `repeat(${width}, auto)`}}>
-        {renderCells({width, height})}
-      </div>
+      <GameBoard width={state.width} height={state.height} />
     </div>
   )
 }
