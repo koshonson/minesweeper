7f63d54797f06c1d4f8c6088026a499bd5013af9
Michael Košatka <michael.kosatka@hrdlicka.cz>
2021-11-10 15:18:06 +0100

    detached GameBoard as a standalone component

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const GameBoard = ({width, height}) => {
+    const renderCells = ({width, height}) => {
+        const cells = [];
+        for (let cell = 0; cell < width * height; cell++) {
+          cells.push(<div className="cell" key={cell}></div>)
+        }
+        return cells;
+      }
+
+    return <div className="game-board" style={{gridTemplateColumns: `repeat(${width}, auto)`}}>
+        {renderCells({width, height})}
+      </div>
+}
