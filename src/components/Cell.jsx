3413db25f5738595afeaaf289b4e70019a1cf23f
Michael Košatka <michael.kosatka@hrdlicka.cz>
2021-11-11 12:39:51 +0100

    hooked gameControls into actual interactions on Cell component level

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 
+import { GAME_STATUSES as game } from '../hooks/useGameStatus';
+
 export const Cell = props => {
-  const { id, explored, flagged, dispatch, neighborBombs, isBomb, exploreArea } = props;
+  const {
+    id,
+    explored,
+    flagged,
+    dispatch,
+    neighborBombs,
+    isBomb,
+    exploreArea,
+    gameStatus,
+    controls
+  } = props;
 
   const flagCell = e => {
     e.preventDefault();
+    if (gameStatus === game.GAME_OVER || gameStatus === game.GAME_WON) return;
     if (explored) return;
     dispatch({ type: 'flag', payload: { id } });
   };
 
   const exploreCell = e => {
     e.preventDefault();
+    if (gameStatus === game.GAME_OVER || gameStatus === game.GAME_WON) return;
+    if (gameStatus === game.READY) controls.startGame();
     if (explored || flagged) return;
     if (isBomb) {
       dispatch({ type: 'explore', payload: { id } });
+      return controls.gameOver();
     }
     const toBeExplored = exploreArea(props);
     dispatch({ type: 'explore-area', payload: { ids: toBeExplored } });
@@ -25,7 +41,13 @@ export const Cell = props => {
       onContextMenu={flagCell}
       onClick={exploreCell}
       style={{
-        backgroundColor: flagged ? 'blue' : explored ? 'rgba(150, 150, 150, 0.5)' : 'dodgerblue'
+        backgroundColor: flagged
+          ? 'blue'
+          : explored
+          ? 'rgba(150, 150, 150, 0.5)'
+          : isBomb
+          ? 'orange'
+          : 'dodgerblue'
       }}
     >
       {explored ? (isBomb ? 'X' : neighborBombs ? neighborBombs : '') : ''}
