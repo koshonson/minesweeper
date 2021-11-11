48f02455dabc9f17ac89f901b718f60dbbde70c2
Michael Košatka <michael.kosatka@hrdlicka.cz>
2021-11-11 12:20:58 +0100

    initialized gameStatus hook on App component level

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './styles/app.css';
 import React from 'react';
 import { useSettings } from './hooks/useSettings.js';
+import { useGameStatus } from './hooks/useGameStatus';
 
 import { GameBoard } from './components/GameBoard.jsx';
 import { SettingsPanel } from './components/SettingsPanel.jsx';
 
 function App() {
-  const [settings, dispatch] = useSettings();
+  const [settings, dispatchSettings] = useSettings();
+  const [gameStatus, setGame] = useGameStatus();
 
   return (
     <div className='container'>
-      <SettingsPanel settings={settings} dispatchSettings={dispatch} />
-      <GameBoard settings={settings} />
+      <SettingsPanel
+        settings={settings}
+        dispatchSettings={dispatchSettings}
+        gameStatus={gameStatus}
+      />
+      <GameBoard settings={settings} setGame={setGame} gameStatus={gameStatus} />
     </div>
   );
 }
