b4e2d1c10a855ff598ab44fe2279e8855a6cc41a
Michael Košatka <michael.kosatka@hrdlicka.cz>
2021-11-12 06:35:05 +0100

    adjusted styling of ControlPanel's settings

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,4 +1,4 @@
-import '../styles/settings.css';
+import '../styles/controls.css';
 import React from 'react';
 import { SETTINGS_ACTIONS as actions } from '../hooks/useSettings';
 
@@ -7,40 +7,61 @@ export const ControlPanel = props => {
   const { height, width, numMines } = settings;
   const explorable = height * width - numMines;
 
+  const renderSettings = () => {
+    return (
+      gameStatus === 'ready' && (
+        <div class='settings'>
+          <button className='start-game' onClick={controls.startGame}>
+            START
+          </button>
+          <div className='settings-inputs'>
+            <label htmlFor='board-width'>Board width:</label>
+            <input
+              type='number'
+              name='board-width'
+              value={settings.width}
+              onChange={e => dispatchSettings({ type: actions.SET_WIDTH, payload: e.target.value })}
+            />
+            <label htmlFor='board-height'>Board height:</label>
+            <input
+              type='number'
+              name='board-height'
+              value={settings.height}
+              onChange={e =>
+                dispatchSettings({ type: actions.SET_HEIGHT, payload: e.target.value })
+              }
+            />
+            <label htmlFor='board-height'>Number of mines:</label>
+            <input
+              type='number'
+              name='num-mines'
+              value={settings.numMines}
+              onChange={e => dispatchSettings({ type: actions.SET_MINES, payload: e.target.value })}
+            />
+          </div>
+        </div>
+      )
+    );
+  };
+
+  const renderControls = () => {
+    return (
+      gameStatus === 'game-on' && (
+        <div className='game-display' style={{ height: `calc(32px * ${height})` }}>
+          <div className='timer'>{explored + '/' + explorable}</div>
+          <div className='timer'>{time}</div>
+          <div className='timer'>{gameStatus}</div>
+          <button onClick={controls.resetGame}>RESET GAME</button>
+        </div>
+      )
+    );
+  };
+
   return (
     <div className={`control-panel${gameStatus === 'game-on' ? ' cp-collapsed' : ''}`}>
       <h1>MineSweeper</h1>
-      {gameStatus !== 'game-on' && (
-        <div className='settings-controls'>
-          <label htmlFor='board-width'>Board width:</label>
-          <input
-            type='number'
-            name='board-width'
-            value={settings.width}
-            onChange={e => dispatchSettings({ type: actions.SET_WIDTH, payload: e.target.value })}
-          />
-          <label htmlFor='board-height'>Board height:</label>
-          <input
-            type='number'
-            name='board-height'
-            value={settings.height}
-            onChange={e => dispatchSettings({ type: actions.SET_HEIGHT, payload: e.target.value })}
-          />
-          <label htmlFor='board-height'>Number of mines:</label>
-          <input
-            type='number'
-            name='num-mines'
-            value={settings.numMines}
-            onChange={e => dispatchSettings({ type: actions.SET_MINES, payload: e.target.value })}
-          />
-        </div>
-      )}
-      <div className='game-display' style={{ height: `calc(32px * ${height})` }}>
-        <div className='timer'>{explored + '/' + explorable}</div>
-        <div className='timer'>{time}</div>
-        <div className='timer'>{gameStatus}</div>
-        <button onClick={controls.resetGame}>RESET GAME</button>
-      </div>
+      {renderSettings()}
+      {renderControls()}
     </div>
   );
 };
